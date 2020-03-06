71e92d5321889dafaf22efb64d25b90cc10dcc0f
Jasmine Harris <jasminecrooms@gmail.com>
2020-03-05 19:21:48 -0800

    updated backend

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.listen(port, ()=> console.log(`listening on port ${port}`));
